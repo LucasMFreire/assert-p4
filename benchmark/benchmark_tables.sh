f9fb92a2fb0539ac66432f4ddd64f4d553fedb1f
LucasMFreire <lucasmefrei@gmail.com>
2017-07-11 17:03:23 -0300

    benchmark update

--- benchmark/benchmark_tables.sh
+++ benchmark/benchmark_tables.sh
@@ -9,7 +9,7 @@ STARTTIME=$(date +%s)
 /home/$USER/p4c/build/p4c-bm2-ss out.p4 --toJSON file.json
 python ../../src/P4_to_C.py file.json > benchmark_model.c
 llvm-gcc -I ../../include -emit-llvm -c -g benchmark_model.c
-klee --search=dfs  benchmark_model.o
+klee --search=dfs --warnings-only-to-file --no-output benchmark_model.o
 
 ENDTIME=$(date +%s)
 ELAPSED_TIME=$(($ENDTIME - $STARTTIME))
