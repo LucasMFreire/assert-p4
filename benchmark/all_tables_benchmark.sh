7f2ee39c2b8eb47a26e3ecc6a1448d71b37b2e0e
LucasMFreire <lucasmefrei@gmail.com>
2017-07-11 13:46:30 -0300

    benchmark update

---
+++ benchmark/all_tables_benchmark.sh
@@ -0,0 +1,60 @@
+bash benchmark_tables.sh 1
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt 
+bash benchmark_tables.sh 2
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt 
+bash benchmark_tables.sh 3
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 4
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 5
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 6
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 7
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 8
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 9
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 10
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 11
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 12
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 13
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 14
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 15
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 16
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 17
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 18
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 19
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 20
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 21
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 22
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 23
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 24
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 25
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 26
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 27
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 28
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 29
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
+bash benchmark_tables.sh 30
+curl --upload-file ./result_tables_P4_benchmark.txt https://transfer.sh/result_tables_P4_benchmark.txt
--- benchmark/benchmark_tables.sh
+++ benchmark/benchmark_tables.sh
@@ -1,8 +1,19 @@
 now=$(date +"%T")
 mkdir $now
 cd $now
-p4benchmark --feature "$@"
+p4benchmark --feature pipeline --tables $1 --table-size 32
+
+STARTTIME=$(date +%s)
+
 /home/lucas/p4c/build/p4test --pp out.p4 --p4-14 output/main.p4
 /home/lucas/p4c/build/p4c-bm2-ss out.p4 --toJSON file.json
 python ../../src/P4_to_C.py file.json > benchmark_model.c
+llvm-gcc -I ../../include -emit-llvm -c -g benchmark_model.c
+klee --search=dfs  benchmark_model.o
+
+ENDTIME=$(date +%s)
+ELAPSED_TIME=$(($ENDTIME - $STARTTIME))
+
 cd ..
+echo $1 $ELAPSED_TIME >> result_tables.txt
+
