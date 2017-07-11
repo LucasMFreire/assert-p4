0bc275182d1f641dfbada1c0a457f440c1bcf61f
LucasMFreire <lucasmefrei@gmail.com>
2017-07-11 14:23:51 -0300

    generalized user in benchmark

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -2,7 +2,7 @@ now=$(date +"%T")
 mkdir $now
 cd $now
 p4benchmark --feature "$@"
-/home/lucas/p4c/build/p4test --pp out.p4 --p4-14 output/main.p4
-/home/lucas/p4c/build/p4c-bm2-ss out.p4 --toJSON file.json
+/home/$USER/p4c/build/p4test --pp out.p4 --p4-14 output/main.p4
+/home/$USER/p4c/build/p4c-bm2-ss out.p4 --toJSON file.json
 python ../../src/P4_to_C.py file.json > benchmark_model.c
 cd ..
