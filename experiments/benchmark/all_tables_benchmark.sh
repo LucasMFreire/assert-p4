ed5447360240dcf5d1611fa80dca78ec0a615ced
LucasMFreire <lucasmefrei@gmail.com>
2017-08-03 14:23:42 -0300

    all_tables_becnhmark for loop

--- experiments/benchmark/all_tables_benchmark.sh
+++ experiments/benchmark/all_tables_benchmark.sh
@@ -1,30 +1,4 @@
-bash benchmark_tables.sh 1
-bash benchmark_tables.sh 2
-bash benchmark_tables.sh 3
-bash benchmark_tables.sh 4
-bash benchmark_tables.sh 5
-bash benchmark_tables.sh 6
-bash benchmark_tables.sh 7
-bash benchmark_tables.sh 8
-bash benchmark_tables.sh 9
-bash benchmark_tables.sh 10
-bash benchmark_tables.sh 11
-bash benchmark_tables.sh 12
-bash benchmark_tables.sh 13
-bash benchmark_tables.sh 14
-bash benchmark_tables.sh 15
-bash benchmark_tables.sh 16
-bash benchmark_tables.sh 17
-bash benchmark_tables.sh 18
-bash benchmark_tables.sh 19
-bash benchmark_tables.sh 20
-bash benchmark_tables.sh 21
-bash benchmark_tables.sh 22
-bash benchmark_tables.sh 23
-bash benchmark_tables.sh 24
-bash benchmark_tables.sh 25
-bash benchmark_tables.sh 26
-bash benchmark_tables.sh 27
-bash benchmark_tables.sh 28
-bash benchmark_tables.sh 29
-bash benchmark_tables.sh 30
+ for i in $(seq 1 30);
+    do
+            bash benchmark_tables.sh $i sefl
+    done
